3846d911c6fbc4b78eb304a1afe2db513aee7c72
Kenji Longid <58013636+kenmalik@users.noreply.github.com>
2024-07-21 21:27:50 -0700

    feat: add deleting

--- src/main/java/com/kmlongid/todo_list/TodoApplication.java
+++ src/main/java/com/kmlongid/todo_list/TodoApplication.java
@@ -4,15 +4,24 @@ import java.util.ArrayList;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @SpringBootApplication
 @Controller
 public class TodoApplication {
-  private static final ArrayList<String> items = new ArrayList<String>();
+
+  private static int currentId = 0;
+
+  private record Item(int id, String content) {
+  };
+
+  private static final ArrayList<Item> items = new ArrayList<Item>();
 
   public static void main(String[] args) {
     SpringApplication.run(TodoApplication.class, args);
@@ -21,9 +30,19 @@ public class TodoApplication {
   @PostMapping("/add")
   public String test(@RequestParam(name = "item", required = true) String item,
       Model model) {
-    items.add(item);
+    items.add(new Item(++currentId, item));
     model.addAttribute("items", items);
     return "list";
+  }
+
+  @DeleteMapping("/delete")
+  @ResponseStatus(HttpStatus.OK)
+  public void delete(@RequestParam(name = "id", required = true) int id) {
+    System.out.println(id);
+    for (Item item : items) {
+      System.out.println(item.id());
+    }
+    items.removeIf(item -> (item.id() == id));
   }
 
 }
